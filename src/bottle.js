ad12c2675b3153e4eea2af47c37d22b590e06916
lightalkmst <rook_master2000@yahoo.com>
2018-06-15 18:18:49 -0400

    bottle question command

--- src/bottle.js
+++ src/bottle.js
@@ -4,12 +4,18 @@ const fs = require ('fs')
 const questions = fs.readFileSync ('src/bottle/questions.txt', 'utf8').split ('\n').filter (x => x)
 
 module.exports = async message => {
+  const ask_question = async () => {
+    const question = questions[Math.floor (Math.random () * questions.length)]
+    await message.channel.send (question)
+  }
+
   util.exec ([[
     /^!spin$/, async match => {
       const target = message.guild.members.filter (user => user.presence.status == 'online' && !user.bot).random ().user
-      const question = questions[Math.floor (Math.random () * questions.length)]
       await message.channel.send (`<@${target.id}> has been chosen to answer a question!`)
-      await message.channel.send (question)
+      await ask_question ()
     },
+  ], [
+    /^!question$/, ask_question,
   ]]) (message)
 }
